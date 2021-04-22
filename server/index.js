d0f92053a9f84cb0729cdc28b66d06542ca27424
Stitchuuuu <stitchuuuu@icloud.com>
2021-04-22 19:56:51 +0200

    most popular coins to use full credit

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const { getCoinList, getQuotes } = require('./coinmarket')
 
 const QUOTES_FOLDER = path.join(__dirname, '..', 'data', 'quotes')
 const DATA_FOLDER = path.join(__dirname, '..', 'data')
-
+const most200PopularCoinsSymbol = []
 async function isWritable(p) {
 	return new Promise(resolve => {
 		fs.access(p, fs.constants.W_OK, (err) => {
@@ -15,7 +15,9 @@ async function isWritable(p) {
 }
 
 async function updateCurrencies() {
-	const data = await getQuotes()
+	let quotes = process.env.SYMBOLS ? process.env.SYMBOLS.split(',') : []
+	quotes = [...quotes, ...most200PopularCoinsSymbol.filter(s => !~quotes.indexOf(s)).slice(0, 100 - quotes.length)]
+	const data = await getQuotes({ symbols: quotes })
 	for (const [symbol, item] of Object.entries(data.quotes)) {
 		fs.writeFileSync(path.join(QUOTES_FOLDER, `${symbol}.json`), JSON.stringify(item))
 	}
@@ -24,6 +26,15 @@ async function updateCurrencies() {
 }
 async function updateCoinList() {
 	const data = await getCoinList()
+	most200PopularCoinsSymbol.splice(0)
+	Object.values(data.$data).sort((a, b) => {
+		if (a.rank < b.rank) return -1
+		else if (a.rank > b.rank) return 1
+		return 0
+	}).slice(0, 200).forEach((c) => {
+		most200PopularCoinsSymbol.push(c.symbol)
+	})
+	
 	fs.writeFileSync(path.join(DATA_FOLDER, 'crypto-list.full.json'), JSON.stringify({ data: data.$data, updatedAt: new Date() }))
 	fs.writeFileSync(path.join(DATA_FOLDER, 'crypto-list.json'), JSON.stringify({ data: data.list, updatedAt: new Date() }))
 }
